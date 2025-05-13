21a57a002a5c859e12acbdaaed9d058c37eb5e92
AASani29 <alfey2001sani@gmail.com>
2025-05-13 12:30:35 +0600

    url changed

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://210042129sdalab01-fqf0ahaxbub8ezew.canadacentral-01.azurewebsites.net/";
+const baseUrl = "https://210042129sdalab01-fqf0ahaxbub8ezew.canadacentral-01.azurewebsites.net";
 
 export default baseUrl;
