2900dc7bd80ce5274fc65716377e942cded1d28f
AASani29 <alfey2001sani@gmail.com>
2025-05-13 12:50:34 +0600

    Dc

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "https://210042129sdalab01-fqf0ahaxbub8ezew.canadacentral-01.azurewebsites.net";
+const baseUrl = "https://210042129sdalab01-fqf0ahaxbub8ezew.canadacentral-01.azurewebsites.net/";
+// const baseUrl = "http://localhost:3001";
 
 export default baseUrl;
